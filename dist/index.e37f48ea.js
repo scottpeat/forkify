4371ed4fecc618ce4f8786d0bfe4fef58d14f4ff
Scott Peat <scottpeatweb@gmail.com>
2022-09-13 08:55:02 +0100

    Getting receipe from API function created

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -545,13 +545,28 @@ const timeout = function(s) {
 ///////////////////////////////////////
 const showRecipe = async function() {
     try {
-        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca57");
         const data = await res.json();
+        if (!res.ok) throw new error(`${data.message} *${res.status})`);
+        console.log(res, data);
+        let recipe = data.data.recipe;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
     } catch (err) {
         alert(err);
     }
 };
 console.log("TEST");
+showRecipe();
 
 },{}]},["fA0o9","aenu9"], "aenu9", "parcelRequire3a11")
 
