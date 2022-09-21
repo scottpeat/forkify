5285d4c7e6fa09443ea738bf6d56d6edf711786c
Scott Peat <scottpeatweb@gmail.com>
2022-09-21 07:32:57 +0100

    Starting work on addRecipeView for uploading recipes

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,9 @@
+import View from './View';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {}
+}
+
+export default new AddRecipeView();
